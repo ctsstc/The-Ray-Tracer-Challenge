dac40df3df4106e9cffa6e07b25f4201536b555f
Cody Swartz <ctsstc@gmail.com>
2019-08-20 03:28:03 -0700

    Add TypeScript & TS Jest

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,9 @@
 // https://jestjs.io/docs/en/configuration.html
 
 module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+
   // All imported modules in your tests should be mocked automatically
   // automock: false,
 
