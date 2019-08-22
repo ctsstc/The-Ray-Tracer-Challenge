b30c188f82f31036b9ef87c234e5bee59bbfe15d
Cody Swartz <ctsstc@gmail.com>
2019-08-22 02:42:15 -0700

    Implement Tuple move Could use a better name

--- src/datatypes/tuple.ts
+++ src/datatypes/tuple.ts
@@ -1,3 +1,10 @@
+import Vector from "./vector";
+
+const TupleType = {
+  point: 0,
+  vector: 1
+}
+
 export default class Tuple {
   constructor(
     public x:number,
@@ -5,4 +12,11 @@ export default class Tuple {
     public z:number,
     public w:number
   ) { }
+
+  public move(vector:Vector):Tuple {
+    const newX = this.x + vector.x;
+    const newY = this.y + vector.y;
+    const newZ = this.z + vector.z;
+    return new Tuple(newX, newY, newZ, TupleType.vector);
+  }
 }
