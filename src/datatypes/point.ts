c5af5d11be4e14bb54a89c702b167b1d2b249fe9
Cody Swartz <ctsstc@gmail.com>
2019-08-25 03:37:20 -0700

    Implement Point.subtract

--- src/datatypes/point.ts
+++ src/datatypes/point.ts
@@ -1,7 +1,15 @@
 import Tuple from "./tuple";
+import Vector from "./vector";
 
 export default class Point extends Tuple {
   constructor(x:number, y:number, z:number) {
     super(x, y, z, 0);
+  }
+
+  public subtract(point:Point):Vector {
+    const newX = this.x - point.x;
+    const newY = this.y - point.y;
+    const newZ = this.z - point.z;
+    return new Vector(newX, newY, newZ);
   }
 }
