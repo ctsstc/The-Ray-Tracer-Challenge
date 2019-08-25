aa24d232cfa36b038d69d07e02880bd78c039980
Cody Swartz <ctsstc@gmail.com>
2019-08-25 04:06:11 -0700

    Implement Vector.subtractVector

--- src/datatypes/vector.ts
+++ src/datatypes/vector.ts
@@ -4,4 +4,9 @@ export default class Vector extends Tuple {
   constructor(x:number, y:number, z:number) {
     super(x, y, z, 1);
   }
+
+  public subtractVector(vector:Vector):Vector {
+    const tuple = this.subtract(vector);
+    return new Vector(tuple.x, tuple.y, tuple.z);
+  }
 }
